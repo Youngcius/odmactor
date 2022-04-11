222f517e9c61f829a005c58fa8f47582383494b5
Zhaohui <zhy@email.arizona.edu>
2022-04-11 13:40:08 -0700

    add tqdm

--- odmactor/scheduler/base.py
+++ odmactor/scheduler/base.py
@@ -14,6 +14,7 @@ import nidaqmx
 import numpy as np
 import scipy.constants as C
 import TimeTagger as tt
+from tqdm import tqdm
 from odmactor.instrument import ASG, Microwave, Laser, LockInAmplifier
 from odmactor.utils import dBm_to_mW, mW_to_dBm
 from typing import List, Any, Optional
@@ -635,7 +636,7 @@ class FrequencyDomainScheduler(Scheduler):
         # ===================================================================
 
         mw_on_seq = self._asg_sequences[self.channel['mw'] - 1]
-        for i, freq in enumerate(self._freqs):
+        for freq in tqdm(self._freqs):
             self._cur_freq = freq
             self.mw.set_frequency(freq)
 
@@ -770,7 +771,7 @@ class TimeDomainScheduler(Scheduler):
                 time.sleep(self.time_pad + self.asg_dwell)
 
         # =======================================================
-        for i, duration in enumerate(self._times):
+        for duration in tqdm(self._times):
             self._cur_time = duration
             self.gene_detect_seq(duration)
             self.asg.start()
